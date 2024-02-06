9ec3b8eeb24e345f446db2a5fe308559dc408023
shimon driham <95618903+shimondriham@users.noreply.github.com>
2024-02-06 16:55:21 +0200

    header

--- src/componentsClient/headerClient.jsx
+++ src/componentsClient/headerClient.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 
 function HeaderClient() {
+  let nav = useNavigate()
+
+  const onLoginClick = () => {
+    nav("/");
+  }
+  const onHomeClick = () => {
+    nav("/home");
+  }
+  const onSignupClick = () => {
+    nav("/signup");
+  }
+
   return (
-    <div>HeaderClient</div>
+    <div>
+      <h1>HeaderClient</h1>
+      <button onClick={onLoginClick}>Login</button>
+      <button onClick={onHomeClick}>Home</button>
+      <button onClick={onSignupClick}>signup</button>
+    </div>
   )
 }
 
