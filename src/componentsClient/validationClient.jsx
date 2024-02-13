40f41635355f80ad1a80cc75ae904b15950bd43d
effiCohen <118920139+effiCohen@users.noreply.github.com>
2024-02-13 20:38:57 +0200

    forgaatpass

--- src/componentsClient/validationClient.jsx
+++ src/componentsClient/validationClient.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
 
 function ValidationClient() {
+  const inputFields = Array.from({ length: 5 }, (_, i) => (
+    <input
+      key={i}
+      type="text"
+      className="w-12 h-10 flex items-center text-center text-black text-base border-2 border-gray-300 focus:border-[#141414] outline-none rounded-lg"
+    />
+  ));
   return (
     <>
       <h1 className="text-center text-[50px] font-sans bg-[#FFFDFB]"> Email Confirm </h1>
@@ -14,13 +21,12 @@ function ValidationClient() {
           <h1 className='font-bolt font-[Poppins] text-[42px]'>Hi! Sharon</h1>
           <h4 className=" text-[32px] font-[Poppins] text-[#141414]">Welcome to bootjob.</h4>
           <p className=" text-lg text-zinc-500"> We’ve sent an Email with an activation code to your mail <span class="font-medium text-[#141414]">fakeOne@ort.il</span>.</p>
-          <div className="flex gap-5 max-w-lg  items-end ">
-            <input type="text" className="w-12 h-10 flex items-center text-center  text-black text-base rounded-lg border-2 border-gray-300 focus:border-[#141414] outline-none " />
-            <input type="text" className="w-12 h-10 flex items-center text-center  text-black text-base border-2 border-gray-300 focus:border-[#141414] outline-none rounded-lg" />
-            <input type="text" className="w-12 h-10 flex items-center text-center  text-black text-base border-2 border-gray-300 focus:border-[#141414] outline-none rounded-lg" />
-            <input type="text" className="w-12 h-10 flex items-center text-center  text-black text-base border-2 border-gray-300 focus:border-[#141414] outline-none rounded-lg" />
-            <input type="text" className="w-12 h-10 flex items-center text-center  text-black text-base border-2 border-gray-300 focus:border-[#141414] outline-none rounded-lg" />
+          <div className="flex gap-5 max-w-lg items-end">
+            {inputFields.map((inputField, index) => (
+              <div key={index}>{inputField}</div>
+            ))}
           </div>
+
           <div className="flex">
             <div className=" px-3 mt-5 text-center">
               <button className="block w-[95%]  bg-[#2E3837] hover:bg-[#FAF7FF] hover:text-[#2E3837]  text-white rounded-2xl px-3 py-3 font-[simple] ">Verify</button>
