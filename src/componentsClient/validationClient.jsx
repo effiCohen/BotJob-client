e86b4958a525c574071b0e1d6530dc9a5a87af52
EladRobin <elad1999@hotmail.com>
2024-02-25 18:59:29 +0200

    ok

--- src/componentsClient/validationClient.jsx
+++ src/componentsClient/validationClient.jsx
@@ -56,26 +56,26 @@ function ValidationClient() {
 
   return (
     <>
-      <h1 className="text-center text-[50px] font-[Poppins] bg-[#FFFDFB]  "> Let’s go! </h1>
 
-      <div className="  flex min-h-screen flex-col items-center justify-between overflow-hidden container bg-[#fffdfbfa] ">
+      <div className="  flex  flex-col items-center justify-between overflow-hidden container bg-[#fffdfbfa] ">
+        <h1 className="text-center text-[34px] font-[Poppins] bg-[#FFFDFB]  "> Let’s go! </h1>
 
-        <figure className='max-w-lg  mx-auto  '>
-          <img src="/src/assets/Asset@2x-8 1.png" alt="email@ " className='h-[30%] w-[50%] mx-auto' />
+        <figure >
+          <img src="/src/assets/Asset@2x-8 1.png" alt="email@ " className='h-[15%] w-[25%] mx-auto max-w-lg   ' />
         </figure>
         <form onSubmit={handleSubmit(send)} className="max-w-xl px-5 text-center flex flex-col  items-center justify-center mb-auto  ">
-          <h1 className='font-bolt font-[inter] text-[42px]'>we just emailed you.</h1>
+          <h1 className='font-bolt font-[inter] text-[30px]'>we just emailed you.</h1>
           <p className=" text-lg text-zinc-500 font-[Poppins]"> We’ve sent an Email with an activation code to your mail <span className="font-extrabold text-[#141414]">{myEmail}</span>.</p>
           <div className="flex gap-5 max-w-lg items-end">
             {/* {inputFields.map((inputField, index) => (
               <div key={index}>{inputField}</div>
                
             ))} */}
-            <input {...codeRef1} type="text" className="w-12 h-10 flex items-center text-center text-black text-base border-2 border-gray-300 focus:border-[#141414] outline-none rounded-lg" />
-            <input {...codeRef2} type="text" className="w-12 h-10 flex items-center text-center text-black text-base border-2 border-gray-300 focus:border-[#141414] outline-none rounded-lg" />
-            <input {...codeRef3} type="text" className="w-12 h-10 flex items-center text-center text-black text-base border-2 border-gray-300 focus:border-[#141414] outline-none rounded-lg" />
-            <input {...codeRef4} type="text" className="w-12 h-10 flex items-center text-center text-black text-base border-2 border-gray-300 focus:border-[#141414] outline-none rounded-lg" />
-            <input {...codeRef5} type="text" className="w-12 h-10 flex items-center text-center text-black text-base border-2 border-gray-300 focus:border-[#141414] outline-none rounded-lg" />
+            <input {...codeRef1} type="text" className="w-10 h-10 flex items-center text-center text-black text-base border-2 border-gray-300 focus:border-[#141414] outline-none rounded-lg" />
+            <input {...codeRef2} type="text" className="w-10 h-10 flex items-center text-center text-black text-base border-2 border-gray-300 focus:border-[#141414] outline-none rounded-lg" />
+            <input {...codeRef3} type="text" className="w-10 h-10 flex items-center text-center text-black text-base border-2 border-gray-300 focus:border-[#141414] outline-none rounded-lg" />
+            <input {...codeRef4} type="text" className="w-10 h-10 flex items-center text-center text-black text-base border-2 border-gray-300 focus:border-[#141414] outline-none rounded-lg" />
+            <input {...codeRef5} type="text" className="w-10 h-10 flex items-center text-center text-black text-base border-2 border-gray-300 focus:border-[#141414] outline-none rounded-lg" />
           </div>
           {errors.code1 ? <small className='text-red-700'>The first number is invalid</small> : ""}
           {errors.code2 ? <small className='text-red-700'>The second number is invalid</small> : ""}
