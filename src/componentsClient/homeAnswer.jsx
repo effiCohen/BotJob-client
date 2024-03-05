6537b2bad89d344fa4dc3ea593c14fad8913ccee
shimon driham <95618903+shimondriham@users.noreply.github.com>
2024-03-05 17:07:28 +0200

    aa

--- src/componentsClient/homeAnswer.jsx
+++ src/componentsClient/homeAnswer.jsx
@@ -1,47 +1,40 @@
 import React, { useState } from 'react';
-import { Textarea } from "@material-tailwind/react";
 
 function HomeAnswer() {
-  const [showTextarea, setShowTextarea] = useState(null);
+  const [showText, setShowText] = useState("text");
 
-  const questions = [
-    { index: 1, text: "Question", icon: "/src/assets/click (1).png" },
-    { index: 2, text: "User Answer", icon: "/src/assets/click (1).png" },
-    { index: 3, text: "Ai Answer", icon: "/src/assets/click (1).png" },
+  const buttons = [
+    { text: "Question", tipe: "question" },
+    { text: "User Answer", tipe: "aiAnswer" },
+    { text: "Ai Answer", tipe: "userAnswer" }
   ];
 
-  const handleButtonClick = (index) => {
-    setShowTextarea(index === showTextarea ? null : index);
+  const onAnswerClick = (index) => {
+    console.log(buttons[index].tipe);
+    // setShowText(buttons[index].tipe);
   };
 
   return (
     <>
-      <header>
-        <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
-          <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-            <img src="/src/assets/return.png" className="mr-3 h-6 sm:h-6" alt="" />
-          </div>
-        </nav>
-      </header>
-
-      <h1 className="text-4xl font-bold leading-tight text-gray-900 sm:text-4xl sm:leading-tight lg:leading-tight lg:text-3xl font-pj mb-3 underline">HomeAnswer</h1>
+      <div className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
+        <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
+          <img src="/src/assets/return.png" className="mr-3 h-6 sm:h-6" alt="" />
+          <h1 className="text-4xl font-bold leading-tight text-gray-900 sm:text-4xl sm:leading-tight lg:leading-tight lg:text-3xl font-pj mb-3 underline">HomeAnswer</h1>
+          <div></div>
+        </div>
+      </div>
 
       <div className="flex items-center justify-center w-[32rem] mx-auto space-x-4">
-        {questions.map(({ index, text, icon }) => (
-          <button
-            key={index}
-            className="flex items-center justify-center w-32 bg-[#2E3837] hover:bg-[#FAF7FF] hover:text-[#2E3837] font-bold text-white rounded-2xl py-3 font-[simple] mt-5 mx-1"
-            onClick={() => handleButtonClick(index)}
-          >
-            {text}
-            {/* <img src={icon} alt={`Your Icon ${index}`} className="w-4 h-4 inline-block ml-2" /> */}
+        {buttons.map((item, index) => (
+          <button onClick={onAnswerClick(index)} key={index} className="bg-black w-full lg:w-[35%] h-[50px] my-3 flex items-center justify-center rounded-xl cursor-pointer relative overflow-hidden transition-all duration-500 ease-in-out shadow-md hover:scale-105 hover:shadow-lg before:absolute before:top-0 before:-left-full before:w-full before:h-full before:bg-gradient-to-r before:from-[#FFFDFB] before:to-[#FBF9F3] before:transition-all before:duration-500 before:ease-in-out before:z-[-1] before:rounded-xl hover:before:left-0 text-[#FBF9F3] hover:text-[#2E3837]">
+            <span className="">{buttons[index].text}</span>
+            <img src="/src/assets/click (1).png" alt="Your Icon" className="w-4 h-4  ml-2 absolute right-2 lg:right-1 lg:flex-none hidden lg:block" />
           </button>
         ))}
       </div>
-
-      {showTextarea !== null && (
+      {showText !== null && (
         <div className="flex flex-col items-center w-[32rem] mx-auto">
-          <Textarea variant="static" placeholder={` ${questions.find(q => q.index === showTextarea).text} :`} rows={8} />
+          <p>{showText}</p>
           <div className="flex w-full justify-between py-1.5">
             <div className="flex gap-2"></div>
           </div>
--- src/componentsClient/homeQushtions.jsx
+++ src/componentsClient/homeQushtions.jsx
@@ -2,9 +2,11 @@ import { React, useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { API_URL, doApiGet } from "../services/apiService";
 import { reverse } from "lodash";
+import { useNavigate } from "react-router-dom";
 
 function HomeQushtions() {
   let [ar, setAr] = useState([]);
+  let nav = useNavigate();
   const ThisInterview = useSelector(state => state.intervewSlice.ThisInterview);
   const temp_ar = [
     "Question 1",
@@ -16,25 +18,30 @@ function HomeQushtions() {
 
   useEffect(() => {
     setAr(temp_ar)
-     doApi()
+    doApi()
   }, [])
 
   const doApi = async () => {
-    let url = API_URL + "/interviews/1interview/"+ThisInterview;
+    let url = API_URL + "/interviews/1interview/" + ThisInterview;
     console.log(url);
     try {
-      let  resData  = await doApiGet(url);  
-        let data = resData.data
-        let dataAr = resData.data.questions
-        console.log(data);
-        console.log(dataAr);
-        reverse(dataAr);
-        setAr(dataAr)
+      let resData = await doApiGet(url);
+      let data = resData.data
+      let dataAr = resData.data.questions
+      console.log(data);
+      console.log(dataAr);
+      reverse(dataAr);
+      setAr(dataAr)
     } catch (error) {
-        console.log(error);
+      console.log(error);
     }
-}
+  }
 
+  const toAnswer = (Qushtion_id) => {
+    console.log(Qushtion_id);
+    // nav("/homeAnswer");
+
+  };
 
   return (
     <>
@@ -44,26 +51,18 @@ function HomeQushtions() {
               overflow: hidden;
             }
         `}
-
       </style>
-
-
-
-
-
       <h1 className="font-[SM-Sans] text-2xl text-[#2E3837]">Learn From Mistakes</h1>
       <p className='text-[#2E3837] text-sm font-[Poppins] text-center ml-6'> We believe in learning from mistakes. <b className="text-[#2E3837]">L.F.M</b></p>
 
       <div className=" mx-auto  lg:px-8 place-items-center   grid grid-rows-2 sm:grid-rows-5 grid-flow-col gap-1">
         {ar.map((item, index) => (
-          <button key={index} className="bg-black w-full lg:w-[35%] h-[50px] my-3 flex items-center justify-center rounded-xl cursor-pointer relative overflow-hidden transition-all duration-500 ease-in-out shadow-md hover:scale-105 hover:shadow-lg before:absolute before:top-0 before:-left-full before:w-full before:h-full before:bg-gradient-to-r before:from-[#FFFDFB] before:to-[#FBF9F3] before:transition-all before:duration-500 before:ease-in-out before:z-[-1] before:rounded-xl hover:before:left-0 text-[#FBF9F3] hover:text-[#2E3837]">
-            <span className="">Qushtion {index+1} id={item}</span>
+          <button onClick={toAnswer(item)} key={index} className="bg-black w-full lg:w-[35%] h-[50px] my-3 flex items-center justify-center rounded-xl cursor-pointer relative overflow-hidden transition-all duration-500 ease-in-out shadow-md hover:scale-105 hover:shadow-lg before:absolute before:top-0 before:-left-full before:w-full before:h-full before:bg-gradient-to-r before:from-[#FFFDFB] before:to-[#FBF9F3] before:transition-all before:duration-500 before:ease-in-out before:z-[-1] before:rounded-xl hover:before:left-0 text-[#FBF9F3] hover:text-[#2E3837]" >
+            <span className="">Qushtion {index + 1} id={item}</span>
             <img src="/src/assets/click (1).png" alt="Your Icon" className="w-4 h-4  ml-2 absolute right-2 lg:right-1 lg:flex-none hidden lg:block" />
           </button>
         ))}
       </div>
-
-
     </>
   );
 }
