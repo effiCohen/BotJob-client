4ae4bd0bb726ec3ee639de0e075020ce4844d8af
EladRobin <elad1999@hotmail.com>
2024-03-12 23:21:40 +0200

    oo

--- src/componentsAdmin/itemAdmin.jsx
+++ src/componentsAdmin/itemAdmin.jsx
@@ -1,35 +1,45 @@
-import React from 'react'
+// ItemAdmin.js
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { addThisIntervewAdmin } from '../featuers/intervewSlice';
 
 function ItemAdmin(props) {
-    const myIntervews = useSelector(state => state.intervewSlice.allMyIntervews);
-    let nav = useNavigate();
-    const dispatch = useDispatch();
-    let item = props.item;
-    let index = props.index;
+  const myIntervews = useSelector((state) => state.intervewSlice.allMyIntervews);
+  let nav = useNavigate();
+  const dispatch = useDispatch();
+  let item = props.item;
+  let index = props.index;
 
-    const toQushtions = () => {
-        dispatch(addThisIntervewAdmin({ ThisInterviewAdmin:item._id }));
-        nav("/admin/interview");
-      };
+  const [isHovered, setIsHovered] = useState(false);
 
-    return (
-        <tr
-            className="border-b transition duration-300 ease-in-out hover:bg-neutral-100 dark:border-neutral-500 dark:hover:bg-neutral-600">
-            <td className="whitespace-nowrap px-4 py-2 font-medium">{index + 1}</td>
-            <td className="whitespace-nowrap px-4 py-2 font-medium">{item.user_fullName}</td>
-            <td className="whitespace-nowrap px-4 py-2">{item.date_created.substring(10, length-1)}</td>
-            <td className="whitespace-nowrap px-4 py-2">{item.job}</td>
-            <td className="whitespace-nowrap px-4 py-2">20:35 min</td>
-            <td className="whitespace-nowrap px-4 py-2">{item.questions.length} - questions</td>
-            <td onClick={toQushtions} className="whitespace-nowrap px-4 py-2">
-    <img src="/src/assets/7740825.gif" alt="GIF" style={{ width: '50px', height: '50px' }} />
-</td>
+  const toQushtions = () => {
+    dispatch(addThisIntervewAdmin({ ThisInterviewAdmin: item._id }));
+    nav('/admin/interview');
+  };
 
-</tr>
-    )
+  return (
+    <tr
+      style={{
+        transition: 'background-color 0.3s ease',
+        borderBottom: '1px solid',
+        borderBottomColor: '#e2e8f0',
+        backgroundColor: isHovered ? '#edf2f7' : 'transparent', // כאשר מועבר העכבר
+      }}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      <td className="whitespace-nowrap px-4 py-2 font-medium">{index + 1}</td>
+      <td className="whitespace-nowrap px-4 py-2 font-medium">{item.user_fullName}</td>
+      <td className="whitespace-nowrap px-4 py-2">{item.date_created.substring(10, length - 1)}</td>
+      <td className="whitespace-nowrap px-4 py-2">{item.job}</td>
+      <td className="whitespace-nowrap px-4 py-2">20:35 min</td>
+      <td className="whitespace-nowrap px-4 py-2">{item.questions.length} - questions</td>
+      <td onClick={toQushtions} className="whitespace-nowrap px-4 py-2">
+        <img src="/src/assets/right-arrow3.png" alt="GIF" style={{ width: '30px', height: '30px' }} />
+      </td>
+    </tr>
+  );
 }
 
-export default ItemAdmin
+export default ItemAdmin;
