79bf5cd2c93fd2aaf284f9beebf200d59004956c
EladRobin <elad1999@hotmail.com>
2024-03-12 23:49:50 +0200

    oo

--- src/componentsClient/homeAnswer.jsx
+++ src/componentsClient/homeAnswer.jsx
@@ -49,8 +49,8 @@ function HomeAnswer() {
 
       <div className="flex items-center justify-center w-[32rem] mx-auto space-x-4">
         {buttons.map((item, index) => (
-          <button onClick={() => onAnswerClick(index)} key={index} className="bg-black w-full lg:w-[35%] h-[50px] my-3 flex items-center justify-center rounded-xl cursor-pointer relative overflow-hidden transition-all duration-500 ease-in-out shadow-md hover:scale-105 hover:shadow-lg before:absolute before:top-0 before:-left-full before:w-full before:h-full before:bg-gradient-to-r before:from-[#FFFDFB] before:to-[#FBF9F3] before:transition-all before:duration-500 before:ease-in-out before:z-[-1] before:rounded-xl hover:before:left-0 text-[#FBF9F3] hover:text-[#2E3837]">
-            <span className="">{buttons[index].text}</span>
+          <button onClick={() => onAnswerClick(index)} key={index} className="bg-black w-full lg:w-[35%] h-[50px] my-3 flex items-center justify-center rounded-xl cursor-pointer relative overflow-hidden transition-all duration-700 ease-in-out shadow-md hover:scale-105 hover:shadow-lg before:absolute before:top-0 before:-left-full before:w-full before:h-full before:bg-gradient-to-r before:from-[#000000] before:to-[#000000] before:transition-all before:duration-500 before:ease-in-out before:z-[-1] before:rounded-xl hover:before:left-0 text-[#ffffff] hover:text-[#ebf7ff]">
+          <span className="">{buttons[index].text}</span>
             <img src="/src/assets/click (1).png" alt="Your Icon" className="w-4 h-4  ml-2 absolute right-2 lg:right-1 lg:flex-none hidden lg:block" />
           </button>
         ))}
