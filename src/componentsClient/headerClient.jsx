1eb59255b171a61c875e4e3332025768091dc65b
effiCohen <118920139+effiCohen@users.noreply.github.com>
2024-03-17 20:02:23 +0200

    xx

--- src/componentsClient/headerClient.jsx
+++ src/componentsClient/headerClient.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { checkTokenLocal } from '../services/localService';
 
+
 function HeaderClient() {
   let nav = useNavigate()
   const [ifTokenExists, setIfTokenExists] = useState(false);
@@ -46,30 +47,32 @@ function HeaderClient() {
           <button onClick={onAdmin}>Admin</button>
           <button >- - -</button>
           <button onClick={onlogout}>logout</button> */}
-          <div className="navbar top-auto">
+          <div className="navbar bg-gray-800 mb-auto top-0 rounded-xl w-full ">
             <div className="navbar-start">
               <div className="dropdown">
-                <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
-                  <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
+                <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                  <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                 </div>
-                <ul tabIndex={0} className="menu menu-sm dropdown-content  z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                  <li ><a onClick={onHomeClick}>Homepage</a></li>
-                  <li ><a onClick={onSetting}>Setting</a></li>
-                  <li ><a onClick={onRestPassword}>Rest Password</a></li>
-                  <li ><a onClick={onAdmin}>Admin</a></li>
+                <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                  <li onClick={onSetting}><a>Setting</a></li>
+                  <li><a>Item 1</a></li>
+                  <li><a>Item 3</a></li>
                 </ul>
               </div>
+              <img src="/src/assets/navlogo.png" onClick={onHomeClick} className='size-14' alt="" />
             </div>
-            <div className="navbar-center">
-              <img src="/src/assets/newLogo (2).jpg" className='size-10' alt="" />
+            <div className="navbar-center hidden lg:flex">
+              <ul className="menu justify-between menu-horizontal px-1">
+                <li onClick={onSetting}><a> Setting <img src="https://i.pinimg.com/originals/50/74/eb/5074eb89bce06dfc710ea21d5c0e2913.png" className='size-6' alt="Icon" /></a></li>
+                <li onClick={onHomeClick}><a>Item 1</a></li>
+
+                <li><a>Item 3</a></li>
+              </ul>
             </div>
             <div className="navbar-end">
-              <button className="btn btn-ghost ">
-                <div className="indicator">
-
-                  <button onClick={onlogout}>logout</button>
-                </div>
-              </button>
+              <ul className='menu justify-between menu-horizontal px-1'>
+                <li onClick={onlogout}><a> Logout <img src="/src/assets/redlogout.png" className='size-6' alt="Icon" /></a></li>
+              </ul>
             </div>
           </div>
         </>
