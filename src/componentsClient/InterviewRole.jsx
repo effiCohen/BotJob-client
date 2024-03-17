54c7afe6902ba25adaf33c0b2817d61ccdb28f14
EladRobin <elad1999@hotmail.com>
2024-03-17 17:39:35 +0200

    aa

--- src/componentsClient/InterviewRole.jsx
+++ src/componentsClient/InterviewRole.jsx
@@ -1,27 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { API_URL, doApiMethod } from '../services/apiService';
 import { addNewIntervew } from '../featuers/newIntervewSlice';
 import { checkSettingLocal } from '../services/settingServis';
-import { Spinner } from '@material-tailwind/react';
-import { useState } from 'react';
 import { toast } from 'react-toastify';
-
+import Spinner from "../Spinner";
 
 function InterviewRole() {
     const [loading, setLoading] = useState(false);
     const dispatch = useDispatch();
     const myName = useSelector(state => state.myDetailsSlice.name);
-    let nav = useNavigate();
-    let seting = {}
-
+    const nav = useNavigate();
+    let setting = {};
 
     const startTheInterview = () => {
         let settingLocal = (checkSettingLocal());
         if (settingLocal) {
-            seting = settingLocal;
-            doApi(seting)
+            setting = settingLocal;
+            doApi(setting);
         } else {
             console.log("Go to settings to select preferences");
             toast.error("Go to settings to select preferences");
@@ -39,33 +36,55 @@ function InterviewRole() {
                 dispatch(addNewIntervew({ newIntervew: resp.data }));
                 nav("/Interview");
             }
-        }
-        catch (err) {
+        } catch (err) {
             console.log(err.response.data);
             setLoading(false);
         }
     }
 
-
     return (
         <>
+             <div class="border-double border-4 border-x-blue-300 border-y-white" > 
+
             <section className="container flex justify-center mx-auto  flex-col w-[80%] lg:w-full items-center">
-                <h1 className="text-center font-[Inter] text-2xl lg:text-xl font-bold text-[#2E3837]">Hello :{myName}</h1>
-                <h2 className="text-center font-[Inter] text-2xl lg:text-xl font-bold text-[#2E3837]"> Before we start let's set some rules</h2>
-                <h2 className="lg:text-start text-center font-[SM-Sans] font-bold text-xl text-bold text-[#2E3837]"> No phone</h2>
-                <p className="text-lg  leading-tight text-red-400  sm:leading-tight lg:leading-tight font-serif mb-3">Don't use the answer phone or as a distraction, in fact put it aside</p>
-                <h2 className="lg:text-start text-center font-[SM-Sans] font-bold text-xl text-bold text-[#2E3837]"> Without google and Chat Gpt</h2>
-                <p className=" text-lg  leading-tight text-red-400  sm:leading-tight lg:leading-tight font-[simple] mb-3">Don't fool yourself you are here to do a professional interview</p>
-                <h2 className="lg:text-start text-center font-[SM-Sans] font-bold text-xl text-bold text-[#2E3837]">Be clear</h2>
-                <p className=" text-lg  leading-tight text-red-400  sm:leading-tight lg:leading-tight font-[simple] mb-3">Give clear answers and be confident</p>
-                <h2 className=" text-center font-[SM-Sans] font-bold text-xl  text-[#2E3837]">Professional tip</h2>
-                <h3 className=" text-center font-[SM-Sans]  text-xl font-semibold text-[#2E3837]">Don't forget to breathe</h3>
-                <button onClick={startTheInterview} className="block w-[90%] max-w-xs mx-auto bg-[#2E3837] hover:bg-[#FAF7FF] hover:text-[#2E3837] font-bold text-white rounded-2xl px-3 py-3 font-[simple] m-5 ">Start</button>
-                { loading ? <Spinner/>:<></>}
-            </section >
+                {loading ? (
+                    <div className="flex justify-center items-center h-screen">
+                        <Spinner />
+                    </div>
+                ) : (
+                    <>
+
+                        <h1 className="text-center font-inter text-2xl lg:text-xl font-bold text-[#2E3837]">Hello :{myName}</h1>
+                        <h2 className="text-center font-inter text-2xl lg:text-xl font-bold text-[#2E3837]"> Before we start let's set some rules</h2>
+                        <h2 className="lg:text-start text-center font-madimi font-bold text-xl text-bold text-[#2E3837]">
+                        No phone 
+                        <img src="/src/assets/smartphone.png" alt="Icon" className="inline-block w-6 h-6 mr-2" />
+                        </h2>
+                        <p className="text-lg  leading-tight text-[#84d3cb]  sm:leading-tight lg:leading-tight font-madimi mb-3 ">Don't use the answer phone or as a distraction, in fact put it aside</p>
+                        <h2 className="lg:text-start text-center font-madimi font-bold text-xl text-bold text-[#2E3837]"> 
+                        Without  Google
+                        <img src="/src/assets/google.png" alt="Google Icon" className="inline-block w-6 h-6 mx-2" /> 
+                        and Chat 
+                        <img src="/src/assets/chat-gpt.png" alt="Chat GPT Icon" className="inline-block w-6 h-6 mx-2" /> 
+                        {/* <img src="/src/assets/google.png" alt="Icon" className="inline-block w-6 h-6 mr-2" /> */}
+                        </h2>
+                        <p className=" text-lg  leading-tight text-[#84d3cb]   sm:leading-tight lg:leading-tight font-madimi mb-3">Don't fool yourself you are here to do a professional interview</p>
+                        <h2 className="lg:text-start text-center font-madimi font-bold text-xl text-bold text-[#2E3837]">Be clear 
+                        <img src="/src/assets/pen.png" alt="Chat GPT Icon" className="inline-block w-6 h-6 mx-2" /> 
+                        </h2>
+                        <p className=" text-lg  leading-tight text-[#84d3cb]   sm:leading-tight lg:leading-tight font-madimi mb-3">Give clear answers and be confident</p>
+                        <h2 className=" text-center font-madimi font-bold text-xl  text-[#2E3837]">Professional tip</h2>
+                        <h3 className=" text-center font-madimi text-xl font-semibold text-[#2E3837]">Don't forget to breathe</h3>
+                        <img src="/src/assets/Animation - 1710626342930.gif" alt="Chat GPT Icon" className="inline-block w-20 h-20 mx-2" /> 
+                        
+                        <button onClick={startTheInterview} className="bg-black w-full lg:w-[35%] h-[50px] my-3 flex items-center justify-center rounded-xl cursor-pointer relative overflow-hidden transition-all duration-700 ease-in-out shadow-md hover:scale-105 hover:shadow-lg before:absolute before:top-0 before:-left-full before:w-full before:h-full before:bg-gradient-to-r before:from-[#000000] before:to-[#000000] before:transition-all before:duration-700 before:ease-in-out before:z-[-1] before:rounded-xl hover:before:left-0 text-[#ffffff] hover:text-[#ebf7ff]"> Start</button>
+                    </>
+                )}
+            </section>
+            </div>
 
         </>
     )
 }
 
-export default InterviewRole
+export default InterviewRole;
--- src/componentsClient/homeQushtions.jsx
+++ src/componentsClient/homeQushtions.jsx
@@ -50,7 +50,12 @@ function HomeQushtions() {
   return (
     <>
      <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-          <img onClick={() => BackToHistory()} src="/src/assets/return.png" className="mr-3 h-6 sm:h-6" alt="" />
+     <button onClick={() => BackToHistory()} class="cursor-pointer duration-200 hover:scale-125 active:scale-100" title="Go Back">
+  <svg xmlns="http://www.w3.org/2000/svg" width="50px" height="50px" viewBox="0 0 24 24" class="stroke-blue-300">
+    <path stroke-linejoin="round" stroke-linecap="round" stroke-width="1.5" d="M11 6L5 12M5 12L11 18M5 12H19"></path>
+  </svg>
+</button>
+          {/* <img onClick={() => BackToHistory()} src="/src/assets/return.png" className="mr-3 h-6 sm:h-6" alt="" /> */}
 
           <div>
           <h1 className="font-[SM-Sans] text-2xl text-[#2E3837]">Interview for the position of :{interview.job}</h1>
