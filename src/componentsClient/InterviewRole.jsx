2d0d059b023a66e9bc10884fa3999514df847ec5
EladRobin <elad1999@hotmail.com>
2024-03-20 01:17:37 +0200

    aa

--- src/componentsClient/InterviewRole.jsx
+++ src/componentsClient/InterviewRole.jsx
@@ -53,27 +53,54 @@ function InterviewRole() {
                 ) : (
                     <div className='flex  items-center  justify-between'> 
                         <div></div>
-                        <div className="w-full lg:w-[40%] text-left lg:text-right">
+                        <div className="w-full lg:w-[40%] text-left lg:text-right m-12">
                             <h1 className="text-left mt-4 text-4xl font-bold leading-tight text-gray-900 sm:text-5xl sm:leading-tight lg:leading-tight lg:text-3xl font-pj m-5">Hello :{myName}</h1>
                             <h2 className="text-left font-inter text-2xl lg:text-xl font-bold text-[#2E3837] mt-2"> Before we start let's set some rules</h2>
-                            <ul className='m-2'>
-                                <li className=''><h2 className="lg:text-start text-left font-madimi font-bold text-xl text-bold text-[#2E3837]">
-                                    No phone
-                                    <img src="/src/assets/smartphone.png" alt="Icon" className="inline-block w-6 h-6 mx-2" />
-                                </h2> </li>
-                                <li className=''> <h2 className="lg:text-start text-left font-madimi font-bold text-xl text-bold text-[#2E3837]">
-                                    Without Google
-                                    <img src="/src/assets/google.png" alt="Google Icon" className="inline-block w-6 h-6 mx-2" />
-                                </h2> </li>
-                                <li className=''><h2 className="lg:text-start text-left font-madimi font-bold text-xl text-bold text-[#2E3837]">
-                                    Without  chat
-                                    <img src="/src/assets/chat-gpt.png" alt="Chat GPT Icon" className="inline-block w-6 h-6 mx-2" />
-                                </h2></li>
-                                <li className=''> <h2 className="lg:text-start text-left font-madimi font-bold text-xl text-bold text-[#2E3837]">Be clear
-                                    <img src="/src/assets/pen.png" alt="Chat GPT Icon" className="inline-block w-6 h-6 mx-2" />
-                                </h2> </li>
-                                <li className=''><h2 className=" text-left font-madimi text-xl font-semibold text-[#2E3837]">Don't forget to breathe</h2> </li>
-                            </ul>
+                            <ul className='m-2' >
+    <li className='flex items-start'> {/* Flex container with items-start aligns children to the start of the cross axis */}
+        <span className="mr-2">&#8226;</span> {/* Dot character */}
+        <h2 className="text-left font-madimi font-bold text-xl text-bold text-[#2E3837]">
+            No phone
+            <img src="/src/assets/no-phone.png" alt="Icon" className="inline-block w-6 h-6 mx-2" />
+        </h2>
+    </li>
+    <li className='flex items-start'> {/* Flex container with items-start aligns children to the start of the cross axis */}
+        <span className="mr-2">&#8226;</span> {/* Dot character */}
+        <h2 className="text-left font-madimi font-bold text-xl text-bold text-[#2E3837]">
+            Without Google
+            <img src="/src/assets/google1.png" alt="Google Icon" className="inline-block w-6 h-6 mx-2" />
+        </h2>
+    </li>
+    <li className='flex items-start'> {/* Flex container with items-start aligns children to the start of the cross axis */}
+        <span className="mr-2">&#8226;</span> {/* Dot character */}
+        <h2 className="text-left font-madimi font-bold text-xl text-bold text-[#2E3837]">
+            Without chat
+            <img src="/src/assets/artificial-intelligence.png" alt="Chat GPT Icon" className="inline-block w-6 h-6 mx-2" />
+        </h2>
+    </li>
+    <li className='flex items-start'> {/* Flex container with items-start aligns children to the start of the cross axis */}
+        <span className="mr-2">&#8226;</span> {/* Dot character */}
+        <h2 className="text-left font-madimi font-bold text-xl text-bold text-[#2E3837]">
+            Be clear
+            <img src="/src/assets/pencil.png" alt="Chat GPT Icon" className="inline-block w-6 h-6 mx-2" />
+        </h2>
+    </li>
+    <li className='flex items-start'> {/* Flex container with items-start aligns children to the start of the cross axis */}
+        <span className="mr-2">&#8226;</span> {/* Dot character */}
+        <h2 className="text-left font-madimi text-xl font-semibold text-[#2E3837]">
+            Don't forget to breathe
+            <img src="/src/assets/difficulty-breathing.png" alt="Chat GPT Icon" className="inline-block w-6 h-6 mx-2" />
+        </h2>
+    </li>
+    <li className='flex items-start'> {/* Flex container with items-start aligns children to the start of the cross axis */}
+        <span className="mr-2">&#8226;</span> {/* Dot character */}
+        <h2 className="text-left font-madimi text-xl font-semibold text-[#2E3837]">
+        Stopwatch activated for test precision
+        <img src="/src/assets/clock.png" alt="Chat GPT Icon" className="inline-block w-6 h-6 mx-2" />
+        </h2>
+    </li>
+</ul>
+
                             <button onClick={startTheInterview} className="bg-black w-full lg:w-[70%] h-[50px] my-3 flex items-center justify-center rounded-xl cursor-pointer relative overflow-hidden transition-all duration-700 ease-in-out shadow-md hover:scale-105 hover:shadow-lg before:absolute before:top-0 before:-left-full before:w-full before:h-full before:bg-gradient-to-r before:from-[#000000] before:to-[#000000] before:transition-all before:duration-700 before:ease-in-out before:z-[-1] before:rounded-xl hover:before:left-0 text-[#ffffff] hover:text-[#ebf7ff] mt-5"> Start</button>
                         </div>
                         <div className="w-full lg:w-[50%]">
@@ -84,6 +111,8 @@ function InterviewRole() {
                 )}
             </section>
 
+            <p className='font-bold'>" They say time changes things but actually you have to change them yourself "</p>
+            <p className='font-bold'> Andy Warhol </p>
 
 
         </>
