3087b4bd17f2904d62b1975b5182522a6cd43f83
EladRobin <elad1999@hotmail.com>
2024-03-24 23:57:30 +0200

    aa

--- src/componentsClient/headerClient.jsx
+++ src/componentsClient/headerClient.jsx
@@ -88,15 +88,15 @@ function HeaderClient() {
         </div>
         <div className="navbar-end">
           <ul className='menu justify-between menu-horizontal px-1'>
-            <button className="group flex items-center justify-start w-10 h-10 bg-night-rider rounded-md shadow-md transition-all duration-300 hover:w-48 bg-white">
+            <button className="group flex items-center justify-start w-10 h-10  rounded-md  transition-all duration-300 hover:w-48 ">
               {/* <svg viewBox="0 0 512 512" className="w-8 h-8 fill-current group-hover:text-white">
                 <path d="M377.9 105.9L500.7 228.7c7.2 7.2 11.3 17.1 11.3 27.3s-4.1 20.1-11.3 27.3L377.9 406.1c-6.4 6.4-15 9.9-24 9.9c-18.7 0-33.9-15.2-33.9-33.9l0-62.1-128 0c-17.7 0-32-14.3-32-32l0-64c0-17.7 14.3-32 32-32l128 0 0-62.1c0-18.7 15.2-33.9 33.9-33.9c9 0 17.6 3.6 24 9.9zM160 96L96 96c-17.7 0-32 14.3-32 32l0 256c0 17.7 14.3 32 32 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l-64 0c-53 0-96-43-96-96L0 128C0 75 43 32 96 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32z"/>
               </svg> */}
               
-              <Lottie options={{ loop: true, autoplay: true, animationData: lottieJson }} height={500} width={500} />
+              {/* <Lottie options={{ loop: true, autoplay: true, animationData: lottieJson }} height={500} width={500} /> */}
 
-              {/* <img src="/src/assets/exit.png" alt="Icon 1" className="h-8 w-8" /> */}
-              <div className="w-3/4 h-full flex items-center justify-center group-hover:opacity-100 text-night-rider opacity-0 transition-opacity duration-300">Logout</div>
+              <img src="/src/assets/log-out.png" alt="Icon 1" className="h-8 w-8" />
+              <div className="w-3/4 h-full flex items-center justify-center group-hover:opacity-100 text-night-rider opacity-0 transition-opacity duration-300 font-bold font-sans">Logout</div>
             </button>
             {/* <li onClick={onlogout}><a> Logout <img src="/src/assets/redlogout.png" className='size-6' alt="Icon" /></a></li> */}
           </ul>
