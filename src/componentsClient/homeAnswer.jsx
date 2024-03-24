62cf14dd11a2317fa7e6d37342bd2c45098ee57d
Sharon Avraham <117310961+sharon14896@users.noreply.github.com>
2024-03-25 00:25:14 +0200

    ans

--- src/componentsClient/homeAnswer.jsx
+++ src/componentsClient/homeAnswer.jsx
@@ -48,14 +48,14 @@ function HomeAnswer() {
               <path stroke-linejoin="round" stroke-linecap="round" stroke-width="1.5" d="M11 6L5 12M5 12L11 18M5 12H19"></path>
             </svg>
           </button> */}
-          <h1 className=" font-bold  text-gray-900 sm:text-4xl   lg:text-2xl font-[Inter] mb-3 ">Learn from answers</h1>
+          <h1 className=" font-bold  text-gray-900 sm:text-3xl   lg:text-2xl font-[Inter] mb-3 ">Learn from answers</h1>
           <div></div>
         </div>
       </div>
 
       <div className="flex items-center justify-between flex-col lg:flex-row w-[32rem] mx-auto space-x-4">
         {buttons.map((item, index) => (
-          <button onClick={() => onAnswerClick(index)} key={index} className="bg-black w-[50%] lg:w-[35%] h-[50px] my-3 flex items-center justify-center rounded-xl cursor-pointer relative overflow-hidden transition-all duration-700 ease-in-out shadow-md hover:scale-105 hover:shadow-lg before:absolute before:top-0 before:-left-full before:w-full before:h-full before:bg-gradient-to-r before:from-[#000000] before:to-[#000000] before:transition-all before:duration-700 before:ease-in-out before:z-[-1] before:rounded-xl hover:before:left-0 text-[#ffffff] hover:text-[#ebf7ff]">
+          <button onClick={() => onAnswerClick(index)} key={index} className="bg-[#ffafcc] w-[50%] lg:w-[35%] h-[50px] my-3 flex items-center justify-center rounded-xl cursor-pointer relative overflow-hidden transition-all duration-700 ease-in-out shadow-md hover:scale-105 hover:shadow-lg before:absolute before:top-0 before:-left-full before:w-full before:h-full before:bg-gradient-to-r before:from-[#3871C1] before:to-[#000000] before:transition-all before:duration-700 before:ease-in-out before:z-[-1] before:rounded-xl hover:before:left-0 text-[#ffffff] hover:text-[#ebf7ff]">
             <span className="">{buttons[index].text}</span>
             <img src="/src/assets/click (1).png" alt="Your Icon" className="w-4 h-4  ml-2 absolute right-2 lg:right-1 lg:flex-none hidden lg:block" />
           </button>
