b0396b4511c19d0c75bcd89066e0d3c14798689c
effiCohen <118920139+effiCohen@users.noreply.github.com>
2024-03-25 00:08:27 +0200

    ok

--- src/componentsAdmin/itemAdmin.jsx
+++ src/componentsAdmin/itemAdmin.jsx
@@ -18,30 +18,29 @@ function ItemAdmin(props) {
   };
 
   return (
- <tr
-  className={`transition-all duration-300 border-b border-gray-200 text-black ${
-    isHovered ? 'bg-gray-200' : 'bg-transparent'
-  }`}
-  onMouseEnter={() => setIsHovered(true)}
-  onMouseLeave={() => setIsHovered(false)}
-  style={{
-    boxShadow: isHovered ? '0 4px 6px rgba(0, 0, 0, 0.1), 0 1px 3px rgba(0, 0, 0, 0.08)' : 'none',
-  }}
->
+    <tr
+      className={`transition-all duration-300 border-b border-gray-200 text-black ${isHovered ? 'bg-[#ffafcc]' : 'bg-transparent'
+        }`}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+      style={{
+        boxShadow: isHovered ? '0 4px 6px rgba(0, 0, 0, 0.1), 0 1px 3px rgba(0, 0, 0, 0.08)' : 'none',
+      }}
+    >
 
-  
+
       <td className="whitespace-nowrap px-4 py-2 font-medium font-[Inter]">{index + 1}</td>
       <td className="whitespace-nowrap px-4 py-2 font-medium font-[Inter]">{item.user_fullName}</td>
       <td className="whitespace-nowrap px-4 py-2 font-[Inter]">{item.date_created.substring(10, length - 1)}</td>
       <td className="whitespace-nowrap px-4 py-2 font-[Inter]">{item.job}</td>
       <td className="whitespace-nowrap px-4 py-2 font-[Inter]">{item.Time}</td>
-      <td className="whitespace-nowrap px-4 py-2 font-[Inter]">{item.questions.length}</td>
+      <td className="whitespace-nowrap px-12 py-2  font-[Inter]">{item.questions.length}</td>
       <td
         onClick={toQushtions}
         className="whitespace-nowrap px-4 py-2"
-        style={{ cursor: 'pointer' }} 
+        style={{ cursor: 'pointer' }}
       >
-        {isHovered && ( 
+        {isHovered && (
           <img src="/src/assets/output.png" alt="GIF" style={{ width: '30px', height: '30px' }} />
         )}
       </td>
