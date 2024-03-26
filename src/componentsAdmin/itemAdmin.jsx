99901dd9d9b777363645eaf2f9a917e02805782e
Sharon Avraham <117310961+sharon14896@users.noreply.github.com>
2024-03-26 18:47:21 +0200

    EMAIL

--- src/componentsAdmin/itemAdmin.jsx
+++ src/componentsAdmin/itemAdmin.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { addThisIntervewAdmin } from '../featuers/intervewSlice';
+import { API_URL, doApiMethod } from '../services/apiService';
 
 function ItemAdmin(props) {
   const myIntervews = useSelector((state) => state.intervewSlice.allMyIntervews);
@@ -17,6 +18,26 @@ function ItemAdmin(props) {
     nav('/admin/interview');
   };
 
+  const deleteInterview = () => {
+    if (confirm("Are you sure you want to delete interview?")) {
+      doApi()
+      window.location.reload();
+    }
+  };
+
+  const doApi = async () => {
+    let url = API_URL + "/interviews/"+item._id;
+    try {
+        let resp = await doApiMethod(url, "DELETE", {});
+        if (resp.data.status = 200) {
+            console.log("Interview deleted");
+        }
+    }
+    catch (err) {
+        console.log(err.response);
+    }
+}
+
   return (
     <tr
       className={`transition-all duration-300 border-b border-gray-200 text-black ${isHovered ? 'bg-[#ffafcc]' : 'bg-transparent'
@@ -31,6 +52,7 @@ function ItemAdmin(props) {
 
       <td className="whitespace-nowrap px-4 py-2 font-medium font-[Inter]">{index + 1}</td>
       <td className="whitespace-nowrap px-4 py-2 font-medium font-[Inter]">{item.user_fullName}</td>
+      <td className="whitespace-nowrap px-12 py-2  font-[Inter]">{item.user_email}</td>
       <td className="whitespace-nowrap px-4 py-2 font-[Inter]">{item.date_created.substring(10, length - 1)}</td>
       <td className="whitespace-nowrap px-4 py-2 font-[Inter]">{item.job}</td>
       <td className="whitespace-nowrap px-4 py-2 font-[Inter]">{item.Time}</td>
@@ -44,12 +66,20 @@ function ItemAdmin(props) {
           <img src="/src/assets/output.png" alt="GIF" style={{ width: '30px', height: '30px' }} />
         )}
       </td>
+      <td
+              onClick={deleteInterview}
+              className="whitespace-nowrap px-4 py-2"
+              style={{ cursor: 'pointer' }}
+            >
       {isHovered && (
               <div className="whitespace-nowrap px-8 ">
 
       <img src="/src/assets/delete.PNG" alt="GIF" style={{cursor: 'pointer',  width: '40px', height: '40px' , margin:'5px' }} />
        </div>
+      
+       
       )}
+ </td>
     </tr>
   );
 }
