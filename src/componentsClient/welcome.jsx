420b737ca3639aa40af5b5f5a932ea0094cd2976
EladRobin <elad1999@hotmail.com>
2024-03-30 23:21:01 +0300

    שש

--- src/componentsClient/welcome.jsx
+++ src/componentsClient/welcome.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import 'tailwindcss/tailwind.css';
-import { Carousel, IconButton } from "@material-tailwind/react";
-import { Card, CardHeader, CardBody, Typography, Avatar } from "@material-tailwind/react";
 import { useNavigate } from 'react-router-dom';
 
 
