3161a318368c6bbfaa09dd2cb68d8b0dc916b611
effiCohen <118920139+effiCohen@users.noreply.github.com>
2024-04-02 19:17:37 +0300

    colors

--- src/componentsClient/signUpClient.jsx
+++ src/componentsClient/signUpClient.jsx
@@ -63,7 +63,7 @@ function SignUpClient() {
                       <label className="text-xs font-[inter] px-1 font-bold text-[#292930] ">First name:</label>
                       <div className="flex">
                         <div className="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i className=" text-gray-400 text-lg"></i></div>
-                        <input {...firstNameRef} type="text" className="w-full text-black -ml-10 pl-10 pr-3 py-1 bg-white rounded-xl text-sm border-2 border-[#292930c6] outline-none focus:border-gray-400" placeholder="Johen" />
+                        <input {...firstNameRef} type="text" className="w-full text-black -ml-10 pl-2  py-1 bg-white rounded-xl text-sm border-2  outline-none border-gray-300" placeholder="Johen" />
                       </div>
                       {errors.FirstName ? <small className='text-red-500'>Enter valid full Name, min 2 chars max 20</small> : ""}
 
@@ -72,7 +72,7 @@ function SignUpClient() {
                       <label className="text-xs font-[Inter] px-1 font-bold text-[#292930] ">Last Name:</label>
                       <div className="flex">
                         <div className="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i className=" text-gray-400 text-lg"></i></div>
-                        <input {...lastNameRef} type="text" className="w-full text-black -ml-10 pl-10 pr-3 py-1 bg-white rounded-xl text-sm border-2 border-[#292930c6] outline-none focus:border-gray-400" placeholder="Due" />
+                        <input {...lastNameRef} type="text" className="w-full text-black -ml-10 pl-2  py-1 bg-white rounded-xl text-sm border-2  outline-none border-gray-300" placeholder="Due" />
                       </div>
                       {errors.LastName ? <small className='text-red-500'>Enter valid full name, min 2 chars max 20</small> : ""}
 
@@ -83,7 +83,7 @@ function SignUpClient() {
                       <label className="text-xs font-[Inter] px-1 font-bold text-[#292930]">Email:</label>
                       <div className="flex">
                         <div className="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i className="mdi mdi-email-outline text-gray-400 text-lg"></i></div>
-                        <input {...emailRef} type="email" className="w-full text-black -ml-10 pl-10 pr-3 py-1 rounded-xl text-sm  border-2 border-[#292930]  bg-white outline-none focus:border-gray-500" placeholder="test@gmail.com" />
+                        <input {...emailRef} type="email" className="w-full text-black -ml-10 pl-2  py-1 bg-white rounded-xl text-sm border-2  outline-none border-gray-300" placeholder="test@gmail.com" />
                       </div>
                       {errors.email ? <small className='text-red-500'>Email invalid</small> : ""}
 
@@ -95,7 +95,7 @@ function SignUpClient() {
                       <label className="text-xs font-[Inter] px-1 font-bold text-[#292930]">Password:</label>
                       <div className="flex">
                         <div className="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i className="mdi mdi-lock-outline text-gray-400 text-lg"></i></div>
-                        <input  {...passwordRef} name='password' type="password" className="w-full text-black -ml-10 pl-10 pr-3 text-sm py-1 rounded-xl border-2   border-[#292930] bg-white outline-none focus:border-gray-500 text-left" placeholder="password" />
+                        <input  {...passwordRef} name='password' type="password" className="w-full text-black -ml-10 pl-2  py-1 bg-white rounded-xl text-sm border-2  outline-none border-gray-300" placeholder="password" />
                       </div>
                       {errors.password ? <small className='text-red-500'>* Enter valid password, min 4 chars</small> : ""}
 
@@ -104,8 +104,8 @@ function SignUpClient() {
                   </div>
                   <div className="flex -mx-3">
                     <div className="w-full px-3 mt-5">
-                      <button className="mt-2 block w-[90%] max-w-xs mx-auto bg-[#ffafcc] hover:bg-[#3871C1]   text-white rounded-2xl px-3 py-3 font-[Inter]">Create an Account</button>
-                      <p className='mx-auto text-center mt-8 text-[#2E3837]  font-[Inter]'>Already have an account? <b onClick={toLogin} className='cursor-pointer'>Sign in </b></p>
+                      <button className="mt-2 block w-[90%] max-w-xs mx-auto bg-[#ba7ea7] hover:bg-[#ba7ea7ec] text-[18px]   text-white rounded-2xl px-3 py-3 font-[Inter]"><b>Create Account</b></button>
+                      <p className='mx-auto text-center mt-8 text-[#2E3837]  font-[Inter]'>Already have an account? <b onClick={toLogin} className='cursor-pointer underline'>Sign in </b></p>
                     </div>
                   </div>
                 </div>
